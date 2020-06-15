752d59faa68695f922d7461aed1a38b00be08623
mikenath223 <michgoldennathan17@gmail.com>
2020-06-15 23:51:06 +0100

    [Update] Include problem solution and pseudocode

--- two_sum.rb
+++ two_sum.rb
@@ -11,3 +11,26 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
 SOLUTION
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  arr = []
+  nums.each_with_index do |n, i|
+    rem = target - n
+    if nums[i+1..-1].include?(rem)
+      arr << nums.index(n)
+      return arr << nums[i+1..-1].index(rem) + i+1
+    end
+  end
+  p ''
+  arr
+end
+p two_sum([3,2,4], 6)
+# [2,7,11,15]  9
+# 2 - 9 = 7
+# if 7 in nums find index of 7
+# push index of 7 and 2 to empty arr
+
+# Return [0, 1]
