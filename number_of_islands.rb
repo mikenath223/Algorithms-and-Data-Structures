bb0e06b27440b99d17e6fad0fb86456bc0c15696
mikenath223 <michgoldennathan17@gmail.com>
2020-06-17 21:19:54 +0100

    [Feature] Add problem statement to number_of_islands algo

---
+++ number_of_islands.rb
@@ -0,0 +1,16 @@
+LINK:
+https://leetcode.com/problems/number-of-islands/submissions/
+
+PROBLEM STATEMENT:
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+
+TEST CASE:
+
+Input:
+    11110
+    11010
+    11000
+    00000
+Output: 1
