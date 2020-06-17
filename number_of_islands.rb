42afc50d623be57ac16fcba327072fe48c979cbf
mikenath223 <michgoldennathan17@gmail.com>
2020-06-17 21:20:39 +0100

    [Update] Include solution to number_of_islands on leet

--- number_of_islands.rb
+++ number_of_islands.rb
@@ -14,3 +14,58 @@ Input:
     11000
     00000
 Output: 1
+
+
+SOLUTION:
+
+# @param {Character[][]} grid
+# @return {Integer}
+def num_islands(grid)
+  return 0 if grid.empty?
+
+m, n = grid.size, grid[0].size
+numb = 0
+cmap = {}
+0.upto(m * n - 1) do |ij|
+  next if cmap[ij]
+
+  i, j = ij.divmod(n)
+  next if grid[i][j] != '1'
+
+  q = [ij]
+  until q.empty?
+    coordinate = q.shift
+    next if cmap[coordinate]
+
+    x, y = coordinate.divmod(n)
+    q << coordinate - n if     x > 0 && grid[x - 1][y] == '1'
+    q << coordinate + n if x + 1 < m && grid[x + 1][y] == '1'
+    q << coordinate - 1 if     y > 0 && grid[x][y - 1] == '1'
+    q << coordinate + 1 if y + 1 < n && grid[x][y + 1] == '1'
+
+    cmap[coordinate] = true
+  end
+
+  numb += 1
+end
+
+numb
+end
+
+
+# 11.divmod(3) //=> [3, 2]
+# 11110
+# 11010
+# 11000
+# 00000 => 1
+
+# 11000
+# 11000
+# 00100
+# 00011 => 3
+
+# m = 4, n = 5
+# numb = 0
+# numb += 1
+
+# queue = q
