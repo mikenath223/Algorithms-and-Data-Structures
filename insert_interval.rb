b50e343d054fbac48ef632591b2f5c4bc101af1d
mikenath223 <michgoldennathan17@gmail.com>
2020-06-23 23:14:17 +0100

    [Update] Add solution for insert interval algo

--- insert_interval.rb
+++ insert_interval.rb
@@ -12,3 +12,34 @@ Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
 Output: [[1,5],[6,9]]
 
 MY SOLUTION:
+
+# @param {Integer[][]} intervals
+# @param {Integer[]} new_interval
+# @return {Integer[][]}
+def insert(intervals, new_interval)
+  return [new_interval] if intervals.length == 0
+  
+  i = 0
+  while i < intervals.length
+    break if intervals[i][0] > new_interval[0]
+    i += 1
+  end
+  intervals.insert(i, new_interval)
+  
+  res = [intervals[0]]
+  
+  for e in intervals
+    if e[0] <= res[-1][1]
+      res[-1][1] = [res[-1][1], e[1]].max
+    else
+      res << e
+    end
+  end
+  p res
+    res
+end
+
+# Check the exact index to insert interval
+# res = []
+# Check for overlapping intervals and merge =>
+# res[-1][1] >= interval[0] return max of both. 
