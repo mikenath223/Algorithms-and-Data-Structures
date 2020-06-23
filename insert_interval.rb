c194e21edae5e43ea84b98e2a1900aecc64d31ca
mikenath223 <michgoldennathan17@gmail.com>
2020-06-23 23:11:55 +0100

    [Feature] Include problem statement for insert interval algo

---
+++ insert_interval.rb
@@ -0,0 +1,14 @@
+LINK:
+https://leetcode.com/problems/insert-interval/
+
+PROBLEM STATEMENT:
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+TEST CASE:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+MY SOLUTION:
