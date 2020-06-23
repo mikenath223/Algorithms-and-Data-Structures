0e7faf90a98ba34afa021d5277b46869fa8f4f86
mikenath223 <michgoldennathan17@gmail.com>
2020-06-23 23:37:46 +0100

    [Feature] Add problem statement for reverse linked list leetcode

---
+++ reverse_linked_list.rb
@@ -0,0 +1,12 @@
+LINK:
+https://leetcode.com/problems/reverse-linked-list/
+
+PROBLEM STATEMENT:
+Reverse a singly linked list.
+
+TEST CASE:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+
+MY SOLUTION:
