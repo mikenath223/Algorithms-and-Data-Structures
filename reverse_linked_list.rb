4fe8c8e252f5cb2831ac03558149bb0437886eeb
mikenath223 <michgoldennathan17@gmail.com>
2020-06-23 23:41:39 +0100

    [Update] Include my solution for reverse linked list

--- reverse_linked_list.rb
+++ reverse_linked_list.rb
@@ -10,3 +10,33 @@ Input: 1->2->3->4->5->NULL
 Output: 5->4->3->2->1->NULL
 
 MY SOLUTION:
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+  return nil if head.nil?
+  prev = nil
+  curr = head
+  
+  while(curr != nil)
+    temp = curr.next
+    curr.next = prev
+    prev = curr
+    curr = temp
+  end
+  head = prev
+  head
+end
+
+# prev = nil
+# curr = head
+# loop breaks when curr == nil
+# temp = curr.next
+# prev = curr
