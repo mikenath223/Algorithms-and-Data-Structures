5afdf39cc6c889f243703b84679b208d752e57d8
mikenath223 <michgoldennathan17@gmail.com>
2020-06-24 22:15:46 +0100

    [Feature] Include problem and test case for task scheduler algo

---
+++ task_scheduler.rb
@@ -0,0 +1,17 @@
+PROBLEM STATEMENT:
+
+You are given a char array representing tasks CPU need to do. It contains capital letters A to Z where each letter represents a different task. Tasks could be done without the original order of the array. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+
+  However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+  
+  You need to return the least number of units of times that the CPU will take to finish all the given tasks.
+  
+  
+  
+TEST CASE:
+  
+  Input: tasks = ["A","A","A","B","B","B"], n = 2
+  Output: 8
+  Explanation: 
+  A -> B -> idle -> A -> B -> idle -> A -> B
+  There is at least 2 units of time between any two same tasks.
