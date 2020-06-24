5ed95372b7ec4ac036be03b8c930ded90912cad0
mikenath223 <michgoldennathan17@gmail.com>
2020-06-24 22:17:46 +0100

    [Update] Include solution for task scheduler algo challenge

--- task_scheduler.rb
+++ task_scheduler.rb
@@ -15,3 +15,46 @@ TEST CASE:
   Explanation: 
   A -> B -> idle -> A -> B -> idle -> A -> B
   There is at least 2 units of time between any two same tasks.
+
+SOLUTION:
+
+# @param {Character[]} tasks
+# @param {Integer} n
+# @return {Integer}
+def least_interval(tasks, n)
+  counts = get_counts_of_tasks(tasks)
+  p counts
+  time = 0
+  while (counts[0] > 0)
+    i = 0
+    while (i <= n)
+      break if counts[0] == 0
+      if (i < counts.length && counts[i] > 0)
+        counts[i] -= 1
+      end
+      time += 1
+      i += 1
+    end
+    counts.sort! {|x,y| -(x <=> y)}
+  end
+  
+  p time
+end
+
+def get_counts_of_tasks(tasks)
+  count = {}
+  tasks.each do |t|
+    count[t] ||= 0
+    count[t] += 1
+  end
+  count.values.sort {|x,y| -x <=> y }
+end
+
+
+p least_interval(["A","A","A","A","A","A","B","C","D","E","F","G"], 2)
+# get_count_of_tasks => array [0,0,0,0,1]
+# loop count[0] > 0
+# inner loop i <= n
+# count.sort
+
+# ['A','B','A','B']
