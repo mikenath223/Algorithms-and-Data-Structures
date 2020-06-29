bd38d428c4411089995a326ba6d6b3bcc287d00f
mikenath223 <michgoldennathan17@gmail.com>
2020-06-29 23:59:26 +0100

    [Update] Include my solution for first bad version algo problem

--- first_bad_version.rb
+++ first_bad_version.rb
@@ -17,3 +17,31 @@ call isBadVersion(5) -> true
 call isBadVersion(4) -> true
 
 Then 4 is the first bad version. 
+
+SOLUTION:
+# The is_bad_version API is already defined for you.
+# @param {Integer} version
+# @return {boolean} whether the version is bad
+# def is_bad_version(version):
+
+# @param {Integer} n
+# @return {Integer}
+def first_bad_version(n)
+  left = 1
+  right = n
+  
+  while left < right
+    mid = left + (right - left) / 2
+    if is_bad_version(mid)
+      right = mid
+    else
+      left = mid + 1
+    end
+  end
+  left
+end
+
+# [1,2...n] 
+
+# n => 9 - 8 - 7 - 6
+# n => 1 - 2 - 3
